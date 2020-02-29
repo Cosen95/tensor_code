fe8ec716f9f54ad3406e205fa4ce266e2012ac82
fengshuan <1263215592@qq.com>
2020-02-29 15:50:33 +0800

    :sparkles: 逻辑回归（logistic-regression）加载二分类数据

---
+++ src/logistic-regression/core.js
@@ -0,0 +1,17 @@
+import * as tf from "@tensorflow/tfjs";
+import * as tfvis from "@tensorflow/tfjs-vis";
+import { getData } from "./data";
+
+window.onload = () => {
+  const data = getData(400);
+
+  tfvis.render.scatterplot(
+    { name: "逻辑回归训练数据" },
+    {
+      values: [
+        data.filter(point => point.label === 1),
+        data.filter(point => point.label === 0)
+      ]
+    }
+  );
+};
